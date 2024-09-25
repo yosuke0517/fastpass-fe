f3fc8546370286bc94d4d5bbf8da6b28d2466bac
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2024-09-25 23:56:59 +0900

    fix: 環境変数

--- src/services/todo/getTodos.ts
+++ src/services/todo/getTodos.ts
@@ -18,7 +18,7 @@ type TodosResponse = {
 export const fetchTodos = async () => {
   // TODO 型, proccess.env.API_FRONT_URLで参照できるようにする
   const response = await apiClient.get<IApiResponse<TodosResponse>>(
-    '/api/todo',
+    `${process.env.API_FRONT_URL}/api/todo`,
     // { cache: 'no-store' },
     // コメントアウトしている場合は.next/cache/fetch-cacheにキャッシュされる
   );
