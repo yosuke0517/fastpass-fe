3d26223d01ae634294d20a4c1e23ce7bac62065f
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2024-09-26 20:37:01 +0900

    fix: cacheの確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-// import { Suspense } from 'react';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import { TodoList } from '@/app/_components/TodoList';
-// import { TodoList2 } from '@/app/_components/TodoList2';
+import { Suspense } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import { TodoList } from '@/app/_components/TodoList';
+import { TodoList2 } from '@/app/_components/TodoList2';
 
 export default function Page() {
   return (
     <main>
       <div className='flex'>
         <p>topです</p>
-        {/*<Suspense fallback={<CircularProgress />}>*/}
-        {/*  <TodoList />*/}
-        {/*</Suspense>*/}
-        {/*<TodoList2 />*/}
+        <Suspense fallback={<CircularProgress />}>
+          <TodoList />
+        </Suspense>
+        <TodoList2 />
       </div>
     </main>
   );
